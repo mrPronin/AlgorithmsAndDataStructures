bfb23dfaba7a77a29902f34ecdfda3653d4220d3
Alexander Pronin <pronin.alx@gmail.com>
2020-01-16 11:23:48 +0100

    Initial Commit

---
+++ AlgorithmsAndDataStructures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Aleksandr Pronin on 16.01.20.
+//  Copyright © 2020 pronin.alx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
